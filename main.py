55dc01ccfc95b81545160b53962245e440d6ed0c
ddag80332-dot <ddag80332@gmail.com>
2026-09-03 08:58:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from flask import Flask
 from threading import Thread
 
 # Bot Credentials
-TOKEN = "8931845974..."
+TOKEN = "8960828504:AAGqVVodgGcIJiF8Lk__E4JL1WFI7vDZ_1k"
+ADMIN_CHAT_ID = "8293715605"
+
 ADMIN_CHAT_ID = "8293715605"
 
 bot = telebot.TeleBot(TOKEN)
